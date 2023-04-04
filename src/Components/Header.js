4d8f140df25f4ac66335db5a96a26e1414012925
IssamLynx <hm_bakha@esi.dz>
2023-04-04 17:07:38 +0200

    Added workflow

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div class="w-full h-16 flex items-center space-x-4">
+    <div className="w-full h-16 flex items-center space-x-4">
       <Link to="/Home">
-        <img src={Logo} alt="Zenika Logo" class="h-full aspect-auto w-[90px]" />
+        <img
+          src={Logo}
+          alt="Zenika Logo"
+          className="h-full aspect-auto w-[90px]"
+        />
       </Link>
-      <a>Valeurs</a>
-      <a>Offres</a>
-      <a>Green IT</a>
-      <a>Sécurité</a>
+      <Link to="/Valeurs">Valeurs</Link>
+      <Link to="/Offres">Offres</Link>
+      <Link to="/GreenIT">Green IT</Link>
+      <Link to="/Securite">Sécurité</Link>
     </div>
   );
 };
